45f8c216b9387cfded961e69e1e6e24064fcbeb1
Efix <efix66@gmail.com>
2022-02-11 14:29:31 +0200

    Finished totDoApp

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import TodoList from "./components/ToDoList";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import TodoInsert from "./components/TodoInsert";
 
-export default function App() {
+const App = () => {
+  // todos: {id: Number, textValue: string, checked: boolean }
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([
+      ...todos,
+      { id: Math.random().toString(), textValue: text, checked: false },
+    ]);
+  };
+
+  const onRemove = (id) => (e) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const onToggle = (id) => (e) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.appTitle}>Hello Todolist</Text>
+      <View style={styles.card}>
+        <TodoInsert onAddTodo={addTodo} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#3143e8",
+  },
+  appTitle: {
+    color: "#fff",
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: "300",
+    textAlign: "center",
+    backgroundColor: "#3143e8",
+  },
+  card: {
+    backgroundColor: "#fff",
+    flex: 1,
+    borderTopLeftRadius: 10, // to provide rounded corners
+    borderTopRightRadius: 10, // to provide rounded corners
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
+
+export default App;
